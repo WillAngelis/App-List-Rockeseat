787a9549563d63b5ab1baa40aa27d0e4e42d56f5
William Angelis <willangelis@gmail.com>
2022-07-15 21:20:31 -0300

    feat: :sparkles: Adicionei propriedades ao componente Card
    Adicionei as propriedades de name e time ao componente Card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,9 @@ export function Home() {
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
-      <Card />
+
+      <Card name="Will" time="10:29:43" />
+      <Card name="Bia" time="15:19:33" />
     </div>
   );
 }

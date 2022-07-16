49e14bb79f07c7d6ecc6a40125d77bd4edb8ea08
William Angelis <willangelis@gmail.com>
2022-07-15 21:45:37 -0300

    feat: :sparkles: Adicionei função para salvar novos estudantes
    Adicionei a função handleAddStudent no botão Adicionar, a função adiciona novos estudantes no state students

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,6 +8,19 @@ export function Home() {
   const [studentName, setStudentName] = useState('');
   const [students, setStudents] = useState([]);
 
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      }),
+    };
+
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
+
   return (
     <div className="container">
       <h1>Lista de Presença</h1>
@@ -18,7 +31,9 @@ export function Home() {
         onChange={(e) => setStudentName(e.target.value)}
       />
 
-      <button type="button">Adicionar</button>
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
 
       {students.map((student) => (
         <Card name={student.name} time={student.time} />
